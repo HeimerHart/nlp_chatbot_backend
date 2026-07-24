029c3ba305f80ca84270f4ce4cc4d9a5f04cde5f
HeimerHart <s.sriansh0405@gmail.com>
2026-07-25 01:55:08 +0530

    route slowapi fixed1

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from models.usermodel import UserRegister,UserLogin
 from middleware.auth_middleware import get_current_user
-from fastapi import Depends
+from fastapi import Request, Depends
 from limiter import limiter
 from controllers.auth_controller import (
     register_user,
@@ -17,6 +17,7 @@ router = APIRouter()
 @router.get("/me")
 @limiter.limit("5/minute")
 async def get_me(
+    request: Request,
     user=Depends(get_current_user)
 ):
     return user
@@ -25,19 +26,24 @@ async def get_me(
 
 @router.post("/api/auth/register")
 @limiter.limit("5/minute")
-async def register(request: UserRegister):
-
+async def register(
+    request: Request,
+    user: UserRegister
+):
     return await register_user(
-        request.email,
-        request.password
+        user.email,
+        user.password
     )
 
 
 @router.post("/api/auth/login")
 @limiter.limit("5/minute")
-async def login(request: UserLogin):
+async def login(
+    request: Request,
+    user: UserLogin
+):
     return await login_user(
-        request.email,
-        request.password
+        user.email,
+        user.password
     )
 
